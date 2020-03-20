7ed832b3a91e15dc94875d31b240da6757e4294e
YChao <290388252@qq.com>
2020-03-20 17:44:09 +0800

    43

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import App from './App'
 import router from './router'
 import './assets/styles/reset.css'
 import './assets/styles/border.css' // 一像素手机端
+import fastClick from 'fastclick'
 
+fastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
