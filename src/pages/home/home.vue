77897706c27b5010df3e567dc4e7103d92b13db1
YChao <290388252@qq.com>
2020-03-23 16:57:44 +0800

    43

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,19 +1,30 @@
 <template>
+<<<<<<< HEAD
   <div>
     <home-header/>
     <home-swiper/>
     <div>test</div>
   </div>
+=======
+    <home-header/>
+>>>>>>> 09a559f2... 43
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+<<<<<<< HEAD
 import HomeSwiper from './components/swiper'
 export default {
   name: 'home',
   components: {
     HomeHeader,
     HomeSwiper
+=======
+export default {
+  name: 'home',
+  components: {
+    HomeHeader: HomeHeader
+>>>>>>> 09a559f2... 43
   }
 }
 </script>
