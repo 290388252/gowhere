c06b0a2da09f3aed89f35a66e1022f44ddd4c618
YChao <290388252@qq.com>
2020-03-26 15:33:25 +0800

    icons

--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -14,45 +14,45 @@
 </template>
 
 <script>
-  export default {
-    name: 'HomeIcons',
-    data () {
-      return {
-        iconList: [
-          {id: '0001', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点11111111111'},
-          {id: '0002', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点2'},
-          {id: '0003', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点3'},
-          {id: '0004', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点4'},
-          {id: '0005', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点5'},
-          {id: '0006', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点6'},
-          {id: '0007', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点7'},
-          {id: '0008', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点8'},
-          {id: '0009', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点9'}
-        ],
-        swiperOption: {
-          pagination: '.swiper-pagination'
-          // some swiper options/callbacks
-          // 所有的参数同 swiper 官方 api 参数
-          // ...
-        }, // 三张轮播，使用变量循环
-        swiperList: []
-      }
-    },
-    computed: {
-      pages () {
-        const pages = []
-        this.iconList.forEach((item, index) => {
-          const page = Math.floor(index / 8)
-          if (!pages[page]) {
-            pages[page] = []
-          }
-          pages[page].push(item)
-        })
-        console.log(pages)
-        return pages
-      }
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {id: '0001', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点11111111111'},
+        {id: '0002', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点2'},
+        {id: '0003', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点3'},
+        {id: '0004', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点4'},
+        {id: '0005', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点5'},
+        {id: '0006', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点6'},
+        {id: '0007', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点7'},
+        {id: '0008', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点8'},
+        {id: '0009', imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png', desc: '热门景点9'}
+      ],
+      swiperOption: {
+        pagination: '.swiper-pagination'
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+      }, // 三张轮播，使用变量循环
+      swiperList: []
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      console.log(pages)
+      return pages
     }
   }
+}
 </script>
 
 <style scoped lang="stylus">
